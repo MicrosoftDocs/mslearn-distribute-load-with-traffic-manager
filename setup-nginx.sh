91bf0ee9479869b1d8aa1463ee5eb4e82aa26090
andygeorgi <57147293+andygeorgi@users.noreply.github.com>
2020-04-02 21:31:39 +0200

    Updated index file path (#3)

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -8,4 +8,4 @@ apt -y install nginx
 rm /var/www/html/index.nginx-debian.html
 
 # Create the default page for the region
-wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-distribute-load-with-traffic-manager/module-update/index.$1.html -O /var/www/html/index.html
+wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-distribute-load-with-traffic-manager/master/index.$1.html -O /var/www/html/index.html
